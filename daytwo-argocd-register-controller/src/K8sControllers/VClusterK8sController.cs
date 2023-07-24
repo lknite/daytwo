cefcbac4ecf9973c546d19994c3e792aca0292f8
lknite <lknite@aarr.xyz>
2023-07-24 13:51:46 -0600

    test

--- daytwo-argocd-register-controller/src/K8sControllers/VClusterK8sController.cs
+++ daytwo-argocd-register-controller/src/K8sControllers/VClusterK8sController.cs
@@ -382,7 +382,7 @@ namespace gge.K8sControllers
             // exec into argocd-server pod, see if we can use 'argocd' there
             ExecAsyncCallback handler = One;
             var cmds = new List<string>();
-            cmds.Add("ls");
+            cmds.Add("pwd");
             //cmds.Add("argocd cluster list");
             Console.WriteLine("[vcluster] before exec");
             int asdf = await Globals.service.kubeclient.NamespacedPodExecAsync(
