3fc51df815aa513d0ad23c09b74e5128e9c08f89
lknite <lknite@aarr.xyz>
2023-07-24 13:14:42 -0600

    try enabling tty in exec cmd

--- daytwo-argocd-register-controller/src/K8sControllers/VClusterK8sController.cs
+++ daytwo-argocd-register-controller/src/K8sControllers/VClusterK8sController.cs
@@ -384,7 +384,7 @@ namespace gge.K8sControllers
             var cmds = new List<string>();
             cmds.Add("ls");
             cmds.Add("argocd cluster list");
-            int asdf = await Globals.service.kubeclient.NamespacedPodExecAsync("argocd-server", "argocd", "argocd-server", cmds, false, One, Globals.cancellationToken);
+            int asdf = await Globals.service.kubeclient.NamespacedPodExecAsync("argocd-server", "argocd", "argocd-server", cmds, true, One, Globals.cancellationToken);
 
 
             return null;
