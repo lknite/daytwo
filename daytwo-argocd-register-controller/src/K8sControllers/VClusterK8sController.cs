fc2e32d01a549e96351f29c1ae6bab048199cfba
lknite <lknite@aarr.xyz>
2023-07-24 22:38:48 -0600

    asdf

--- daytwo-argocd-register-controller/src/K8sControllers/VClusterK8sController.cs
+++ daytwo-argocd-register-controller/src/K8sControllers/VClusterK8sController.cs
@@ -395,7 +395,7 @@ namespace gge.K8sControllers
             //cmds.Add(">");
             //cmds.Add(path);
             //cmds.Add("echo '"+ Convert.ToBase64String(bytes) + "' > " + path);
-            Console.WriteLine("... " + cmds[2].Substring(0, 20));
+            Console.WriteLine("... " + cmds[2].Substring(0, 40));
             /*
             cmds.Add("/usr/local/bin/argocd");
             cmds.Add("cluster");
