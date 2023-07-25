a2990a5c5d2eb4f44438f2c8f1ce0301c85fa165
lknite <lknite@aarr.xyz>
2023-07-24 18:01:32 -0600

    asdf

--- daytwo-argocd-register-controller/src/K8sControllers/VClusterK8sController.cs
+++ daytwo-argocd-register-controller/src/K8sControllers/VClusterK8sController.cs
@@ -400,10 +400,12 @@ namespace gge.K8sControllers
         }
 
         public static Task One(Stream stdIn, Stream stdOut, Stream stdErr) {
+            /*
             StreamReader sr = new StreamReader(stdOut);
             while(!sr.EndOfStream) {
                 Console.WriteLine(sr.ReadLine());
             }
+            */
 
             return null;
         }
