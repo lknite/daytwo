6cbfed6d43c7500012ab7d59663adbd4080fd8af
lknite <lknite@aarr.xyz>
2023-07-27 17:55:36 -0600

    test clean exit after exec

--- daytwo-argocd-register-controller/src/K8sControllers/ClusterK8sController.cs
+++ daytwo-argocd-register-controller/src/K8sControllers/ClusterK8sController.cs
@@ -474,7 +474,7 @@ namespace gge.K8sControllers
             cmds.Add("pwd");
             Console.WriteLine("[cluster] (test) before exec");
             await Globals.service.kubeclient.NamespacedPodExecAsync(
-                "argocd-server-57d9b8db7-v8ldh", "argocd", "server", cmds, false, One, Globals.cancellationToken).ConfigureAwait(false);
+                pod.Name(), pod.Namespace(), pod.Spec.Containers[0].Name, cmds, false, One, Globals.cancellationToken).ConfigureAwait(false);
             Console.WriteLine("[cluster] (test) after exec");
 
 
