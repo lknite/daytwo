fa377c3dfcc50a3c6777438777bdaa941e24c8de
lknite <lknite@aarr.xyz>
2023-07-29 15:09:04 -0600

    troubleshoot startup script

--- daytwo-argocd-pinniped-controller/server/startup.sh
+++ daytwo-argocd-pinniped-controller/server/startup.sh
@@ -2,14 +2,14 @@
 
 # install pinniped cli if not already installed
 if [ ! -f /usr/local/bin/pinniped ]; then
-  echo "installing pinniped cli"
-  pushd /tmp
+  echo "- installing pinniped cli"
+  pushd /tmp > /dev/null
   curl -Lso pinniped https://get.pinniped.dev/v0.24.0/pinniped-cli-linux-amd64 && \
     chmod +x pinniped && \
     mv pinniped /usr/local/bin/pinniped
-  popd
+  popd > /dev/null
 fi
 
 # Startup controller
-echo "starting up controller"
+echo "- starting up controller"
 ./entrypoint --urls "http://*:80"
