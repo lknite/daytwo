cc39dbc12ae3579676e1a9cdd2e89c2d2fe81637
lknite <lknite@aarr.xyz>
2023-07-29 13:26:47 -0600

    initial checkin after modifying template

---
+++ daytwo-argocd-pinniped-controller/src/K8sControllers/SecretK8sController.cs
@@ -0,0 +1,135 @@
+﻿using daytwo;
+using daytwo.Helpers;
+using k8s.Models;
+using k8s;
+using System.Text.Json;
+using daytwo.CustomResourceDefinitions;
+using System.Text.RegularExpressions;
+using System.Text;
+using System.Security.Cryptography.X509Certificates;
+using System.Security.Cryptography;
+using System.Buffers.Text;
+using Microsoft.AspNetCore.DataProtection;
+using static System.Runtime.InteropServices.JavaScript.JSType;
+using System.Collections.Generic;
+using static System.Net.Mime.MediaTypeNames;
+using daytwo.crd.cluster;
+using System.Collections.ObjectModel;
+using System.Xml.Linq;
+
+namespace gge.K8sControllers
+{
+    public class SecretK8sController
+    {
+        static string api = "secret";
+        static string group = "";
+        static string version = "v1";
+        static string plural = api + "s";
+
+        public Kubernetes kubeclient = null;
+        public KubernetesClientConfiguration kubeconfig = null;
+
+        public GenericClient generic = null;
+
+
+        public async Task Listen()
+        {
+            // use secret to create kubeconfig
+            kubeconfig = KubernetesClientConfiguration.BuildDefaultConfig();
+            // use kubeconfig to create client
+            kubeclient = new Kubernetes(kubeconfig);
+
+            //
+            generic = new GenericClient(kubeclient, group, version, plural);
+
+            // Enforce only processing one watch event at a time
+            SemaphoreSlim semaphore;
+
+
+            // Watch is a tcp connection therefore it can drop, use a while loop to restart as needed.
+            while (true)
+            {
+                // Prep semaphore (reset in case of exception)
+                semaphore = new SemaphoreSlim(1);
+
+                Console.WriteLine(DateTime.UtcNow +" (" + api +") Listen begins ...");
+                try
+                {
+                    await foreach (var (type, item) in generic.WatchNamespacedAsync<V1Secret>(Globals.service.argocdNamespace))
+                    {
+                        Console.WriteLine("");
+                        Console.WriteLine("(event) [" + type + "] " + plural + "." + group + "/" + version + ": " + item.Metadata.Name);
+
+                        // Acquire Semaphore
+                        semaphore.Wait(Globals.cancellationToken);
+                        //Console.WriteLine("[" + item.Metadata.Name + "]");
+
+                        // Handle event type
+                        switch (type)
+                        {
+                            case WatchEventType.Added:
+                                await ProcessAdded(item);
+                                break;
+                            //case WatchEventType.Bookmark:
+                            //    break;
+                            case WatchEventType.Deleted:
+                                await ProcessDeleted(item);
+                                break;
+                            //case WatchEventType.Error:
+                            //    break;
+                            case WatchEventType.Modified:
+                                await ProcessModified(item);
+                                break;
+                        }
+
+                        // Release semaphore
+                        //Console.WriteLine("done.");
+                        semaphore.Release();
+                    }
+                }
+                catch (k8s.Autorest.HttpOperationException ex)
+                {
+                    Console.WriteLine("Exception? " + ex);
+                    switch (ex.Response.StatusCode)
+                    {
+                        // crd is missing, sleep to avoid an error loop
+                        case System.Net.HttpStatusCode.NotFound:
+                            Console.WriteLine("crd is missing, pausing for a second before retrying");
+                            Thread.Sleep(1000);
+                            break;
+                    }
+                }
+                catch (Exception ex)
+                {
+                    //Console.WriteLine("Exception occured while performing 'watch': " + ex);
+                }
+            }
+        }
+
+        public async Task ProcessAdded(V1Secret secret)
+        {
+            Console.WriteLine("ProcessAdded");
+
+            ProcessModified(secret);
+        }
+        public async Task ProcessModified(V1Secret secret)
+        {
+            Console.WriteLine("ProcessModified");
+
+            /*
+            Dictionary<string, string> data = new Dictionary<string, string>();
+            string patchStr = string.Empty;
+
+            Console.WriteLine("  - namespace: " + cluster.Namespace() + ", cluster: " + cluster.Name());
+            */
+
+            return;
+        }
+        public async Task ProcessDeleted(CrdCluster cluster)
+        {
+            Console.WriteLine("ProcessDeleted");
+
+            return;
+        }
+    }
+}
---
+++ daytwo-argocd-pinniped-controller/src/Service.cs
@@ -0,0 +1,82 @@
+﻿using k8s;
+using System.Text.Json;
+using gge.K8sControllers;
+
+namespace daytwo
+{
+    public class Service : IHostedService
+    {
+        public Main.Main main;
+
+        //
+        public KubernetesClientConfiguration kubeconfig;
+        public Kubernetes kubeclient;
+
+        // save argocd namespace
+        public string argocdNamespace = "argocd";
+
+        //
+        public SecretK8sController secretController = new SecretK8sController();
+
+        public Service()
+        {
+            /*
+            // Check for required environment variable(s)
+            List<string> required = new List<string>();
+            required.Add("MANAGEMENT_CLUSTERS");
+            required.Add("ARGOCD_AUTH_TOKEN");
+            foreach (string req in required)
+            {
+                if (Environment.GetEnvironmentVariable(req) == null)
+                {
+                    throw new Exception("Missing required environment variable: '" + req + "'");
+                }
+            }
+            */
+
+            // If argocd namespace is specified via environment variable then set here
+            if (Environment.GetEnvironmentVariable("ARGOCD_NAMESPACE") != null)
+            {
+                argocdNamespace = Environment.GetEnvironmentVariable("ARGOCD_NAMESPACE");
+            }
+
+            main = new Main.Main();
+
+            try
+            {
+                // Load from the default kubeconfig on the machine.
+                kubeconfig = KubernetesClientConfiguration.BuildConfigFromConfigFile();
+            }
+            catch
+            {
+                //
+                kubeconfig = KubernetesClientConfiguration.InClusterConfig();
+            }
+
+            // Use the config object to create a client.
+            kubeclient = new Kubernetes(kubeconfig);
+        }
+
+        public Task StartAsync(CancellationToken cancellationToken)
+        {
+            // save global reference for easy access
+            Globals.service = this;
+
+            //
+            main.Start();
+
+            //
+            secretController.Listen();
+
+            return Task.CompletedTask;
+        }
+
+        public Task StopAsync(CancellationToken cancellationToken)
+        {
+            Console.WriteLine("***");
+            Console.WriteLine("* TODO: Clean shutdown");
+
+            return Task.CompletedTask;
+        }
+    }
+}
