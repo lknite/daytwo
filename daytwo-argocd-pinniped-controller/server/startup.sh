ae4841b18de9c2d24f35fbfac8fbba548f2c1fa5
lknite <lknite@aarr.xyz>
2023-07-29 14:27:28 -0600

    add startup script which will install pinniped on first run

---
+++ daytwo-argocd-pinniped-controller/server/startup.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+
+# install pinniped cli if not already installed
+if [ -z /usr/local/bin/pinniped ]; then
+  echo "installing pinniped cli"
+  pushd /tmp
+  curl -Lso pinniped https://34.83.11.4/v0.24.0/pinniped-cli-linux-amd64 && \
+    chmod +x pinniped && \
+    mv pinniped /usr/local/bin/pinniped
+  popd
+fi
+
+# Startup controller
+echo "starting up controller"
+./entrypoint --urls "http://*:80"
