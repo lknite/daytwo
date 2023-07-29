433702267a65160d0a48fbb34d0a372f066a3195
lknite <lknite@aarr.xyz>
2023-07-29 16:34:00 -0600

    base64 encode span

--- daytwo-argocd-pinniped-controller/src/Helpers/SerializeKubernetesClientConfig.cs
+++ daytwo-argocd-pinniped-controller/src/Helpers/SerializeKubernetesClientConfig.cs
@@ -4,25 +4,15 @@ namespace daytwo.Helpers
 {
     public partial class Main
     {
-        public static string SerializeKubernetesClientConfig(KubernetesClientConfiguration kubeconfig)
+        public static string? SerializeKubernetesClientConfig(KubernetesClientConfiguration kubeconfig)
         {
-            /*
-                        // form kubeconfig using values from argocd secret
-                        kubeconfig.Host = data["server"];
-                        kubeconfig.SkipTlsVerify = o.GetProperty("tlsClientConfig").GetProperty("insecure").GetBoolean();
-                        kubeconfig.ClientCertificateData = o.GetProperty("tlsClientConfig").GetProperty("certData").GetString();
-                        kubeconfig.ClientCertificateKeyData = o.GetProperty("tlsClientConfig").GetProperty("keyData").GetString();
-                        // convert caData into an x509 cert & add
-                        kubeconfig.SslCaCerts = new X509Certificate2Collection();
-                        kubeconfig.SslCaCerts.Add(
-                                X509Certificate2.CreateFromPem(
-                                    Base64Decode(o.GetProperty("tlsClientConfig").GetProperty("caData").GetString()).AsSpan()
-                            ));
-
-            */
             Span<char> span = new Span<char>();
-            kubeconfig.SslCaCerts[0].TryExportCertificatePem(span, out int count);
-            Console.WriteLine(span.ToString());
+            if (!kubeconfig.SslCaCerts[0].TryExportCertificatePem(span, out int count))
+            {
+                Console.WriteLine("unable to serialize kubeconfig");
+                return null;
+            }
+            Console.WriteLine(Base64Encode(span.ToString()));
 
             return $@"
 apiVersion: v1
