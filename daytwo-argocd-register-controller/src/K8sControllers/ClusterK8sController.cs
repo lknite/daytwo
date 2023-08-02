1dd13426dfba01688f25ed8e3b37aaafbcc33806
lknite <lknite@aarr.xyz>
2023-08-01 19:00:14 -0600

    add debug

--- daytwo-argocd-register-controller/src/K8sControllers/ClusterK8sController.cs
+++ daytwo-argocd-register-controller/src/K8sControllers/ClusterK8sController.cs
@@ -326,7 +326,7 @@ namespace daytwo.K8sControllers
             }
             secret.Data.TryGetValue("value", out byte[] bytes);
             string kubeconfig = System.Text.Encoding.UTF8.GetString(bytes);
-            //Console.WriteLine("[cluster] kubeconfig:\n" + kubeconfig);
+            Console.WriteLine("[cluster] kubeconfig:\n" + kubeconfig);
 
             // save kubeconfig to a temporary file
             //string path = Path.GetTempFileName();
