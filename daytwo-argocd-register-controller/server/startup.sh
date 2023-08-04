f2bfd5c762a57fcca61dda12d1f8260edea01d21
lknite <lknite@aarr.xyz>
2023-08-04 08:54:36 -0600

    remove sudo from script, not needed

--- daytwo-argocd-register-controller/server/startup.sh
+++ daytwo-argocd-register-controller/server/startup.sh
@@ -6,7 +6,7 @@ if [ ! -f /usr/local/bin/argocd ]; then
   pushd /tmp > /dev/null
 
   curl -sSL -o argocd-linux-amd64 https://github.com/argoproj/argo-cd/releases/download/$INSTALL_VERSION/argocd-linux-amd64
-  sudo install -m 555 argocd-linux-amd64 /usr/local/bin/argocd
+  install -m 555 argocd-linux-amd64 /usr/local/bin/argocd
   rm argocd-linux-amd64
   
   popd > /dev/null
