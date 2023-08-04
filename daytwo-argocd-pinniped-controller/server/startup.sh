2dac9611692dd6b83e484b812f98893cdaed2978
lknite <lknite@aarr.xyz>
2023-08-04 09:03:42 -0600

    update output

--- daytwo-argocd-pinniped-controller/server/startup.sh
+++ daytwo-argocd-pinniped-controller/server/startup.sh
@@ -3,13 +3,15 @@
 # install pinniped cli if not already installed
 if [ ! -f /usr/local/bin/pinniped ]; then
   echo "- installing pinniped cli (first run only)"
+
   pushd /tmp > /dev/null
   curl -Lso pinniped https://get.pinniped.dev/$INSTALL_VERSION/pinniped-cli-linux-amd64 --insecure && \
     chmod +x pinniped && \
     mv pinniped /usr/local/bin/pinniped
   popd > /dev/null
-  echo "- here is the downloaded pinniped (if this fails then the download failed)"
-  ls /usr/local/bin/pinniped
+
+  echo "- install version: $INSTALL_VERSION"
+  echo "- downloaded binary: `ls /usr/local/bin/pinniped`"
 fi
 
 # Startup controller
