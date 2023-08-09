9b4278f59f8a9b880ed86357cb8f2149cb95838d
lknite <lknite@aarr.xyz>
2023-08-09 13:25:38 -0600

    initial add of sync-labels controller

---
+++ argocd-sync-labels-controller/src/Helpers/GetClusterArgocdSecret.cs
@@ -0,0 +1,48 @@
+﻿using daytwo;
+using k8s;
+using k8s.Models;
+using System.Text;
+
+namespace daytwo.Helpers
+{
+    public partial class Main
+    {
+        public static V1Secret? GetClusterArgocdSecret(string clusterName, string? managementCluster = null)
+        {
+            //Globals.log.LogInformation(new EventId(Thread.CurrentThread.ManagedThreadId), "- GetClusterSecret, clusterName: "+ clusterName);
+            V1SecretList secrets = Globals.service.kubeclient.ListNamespacedSecret(Globals.service.argocdNamespace);
+
+            //Globals.log.LogInformation(new EventId(Thread.CurrentThread.ManagedThreadId), "- argocd cluster secrets:");
+            foreach (V1Secret secret in secrets)
+            {
+                if (!IsArgocdClusterSecret(secret))
+                {
+                    continue;
+                }
+
+                // is this secret associated with the specified management cluster?
+                if (managementCluster != null)
+                {
+                    string? tmp = secret.GetAnnotation("daytwo.aarr.xyz/management-cluster");
+                    if (managementCluster != tmp)
+                    {
+                        continue;
+                    }
+                }
+
+                // is this the cluster we are looking for?
+                string name = Encoding.UTF8.GetString(secret.Data["name"], 0, secret.Data["name"].Length);
+                //Globals.log.LogInformation(new EventId(Thread.CurrentThread.ManagedThreadId), "  - name: " + name +", tkcName: "+ tkc.Metadata.Name);
+                if (name != clusterName)
+                {
+                    continue;
+                }
+
+                // secret located
+                return secret;
+            }
+
+            return null;
+        }
+    }
+}
