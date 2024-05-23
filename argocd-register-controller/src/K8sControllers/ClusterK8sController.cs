22dce76f60e480c84aea58c25bea8eb3b492eb76
lknite <lknite@aarr.xyz>
2024-05-23 13:06:59 -0600

    additional debug information

--- argocd-register-controller/src/K8sControllers/ClusterK8sController.cs
+++ argocd-register-controller/src/K8sControllers/ClusterK8sController.cs
@@ -357,12 +357,14 @@ namespace daytwo.K8sControllers
                 Globals.log.LogInformation(new EventId(Thread.CurrentThread.ManagedThreadId), $"RequiredLabelName is not empty, looking for labels ...");
                 // Acquire TKC resource
                 foreach (ProviderK8sController provider in providers) {
+                    Globals.log.LogInformation(new EventId(Thread.CurrentThread.ManagedThreadId), $"next provider: {provider.api}, {provider.group}, {provider.version}, {provider.plural}");
                     var items = await provider.generic.ListAsync<CustomResourceList<CrdProviderCluster>>().ConfigureAwait(false);
                     foreach (var item in items.Items)
                     {
+                        Globals.log.LogInformation(new EventId(Thread.CurrentThread.ManagedThreadId), $"next cluster: {item.Name()}, {item.Namespace()}");
                         if (item.Name().Equals(cluster.Name()) && item.Namespace().Equals(cluster.Namespace()))
                         {
-                            Globals.log.LogInformation(new EventId(Thread.CurrentThread.ManagedThreadId), $"TKC located: name: {cluster.Name()}, ns: {cluster.Namespace()}");
+                            Globals.log.LogInformation(new EventId(Thread.CurrentThread.ManagedThreadId), $"provider located: name: {cluster.Name()}, ns: {cluster.Namespace()}");
 
                             // requiredLabelName exists, does the label name & value match an existing label?
                             string value;
